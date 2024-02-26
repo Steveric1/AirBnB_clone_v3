a7ff361b05c841512d462ac22682b991956c7c8d
Eric <Stephen.erich243@gmail.com>
2024-02-26 08:52:35 +0100

    updated

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """Create a new Flask app instance."""
 
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 import os
@@ -14,6 +14,12 @@ app.register_blueprint(app_views)
 def teardown_db(self):
     """Closes the storage on teardown"""
     storage.close()
+
+
+@app.errorhandler(404)
+def not_found(error):
+    """Return a JSON-formatted 404 status code response."""
+    return jsonify({"error": "Not found"}), 404
 
 
 if __name__ == "__main__":
