804991f73525d0e3603a7196a8aac82612100a9d
Eric <Stephen.erich243@gmail.com>
2024-02-26 08:37:47 +0100

    update

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,7 +2,7 @@
 
 """create a route /status on the object app_views
 that returns a JSON: "status": "OK" """
-
+from models import storage
 from flask import jsonify
 from api.v1.views import app_views
 
@@ -11,3 +11,16 @@ from api.v1.views import app_views
 def status():
     """Return a JSON status: OK"""
     return jsonify(status="OK")
+
+
+@app_views.route('/stats', strict_slashes=False)
+def stats():
+    """Endpoint that retrieves the number of each objects by type"""
+    classes = {
+        "amenities": "Amenity", "cities": "City",
+        "places": "Place", "reviews": "Review",
+        "states": "State", "users": "User"
+    }
+    for key in classes:
+        classes[key] = storage.count(classes[key])
+    return jsonify(classes)
