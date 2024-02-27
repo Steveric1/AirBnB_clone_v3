d8ce66531d8a5507064521a5cb9774006ca88db5
Eric <Stephen.erich243@gmail.com>
2024-02-27 10:20:12 +0100

    updated

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -5,7 +5,7 @@ from models.user import User
 from models import storage
 from flask import jsonify, abort, make_response, request
 from api.v1.views import app_views
-from flasgger import swag_from
+from flasgger.utils import swag_from
 
 
 @app_views.route('/users', methods=['GET'], strict_slashes=False)
